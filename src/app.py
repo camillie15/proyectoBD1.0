902b36b83a0002e094da5b40f14b4f6d2c8d956a
camillie15 <138990540+camillie15@users.noreply.github.com>
2024-01-09 23:07:41 -0500

    update app.py

--- src/app.py
+++ src/app.py
@@ -1,4 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+from conf import config
 
+app = Flask(__name__, static_folder='static', template_folder='template')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/tusEventosPage')
+def tusEventosPage():
+    return render_template('index1.html')
+
+if __name__ == '__main__':
+    app.config.from_object(config['development'])
+    app.run()
